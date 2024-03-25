d07e0dcecc9548bde78a221532f079ae48f71dbc
MayamaTakeshi <mayamatakeshi@gmail.com>
2024-03-25 14:05:46 +0900

    Updated examples/from_wav.js

--- examples/from_wav.js
+++ examples/from_wav.js
@@ -8,9 +8,14 @@ const format = {
 	channels: 1,
 }
 
-var file_path = __dirname + '/artifacts/digits.1234.wav'
+var file_path
+
 if(process.argv[2]) {
 	file_path = process.argv[2]
+	console.log(`file_path=${file_path}`)
+} else {
+	file_path = __dirname + '/artifacts/digits.1234.8000hz.wav'
+	console.log(`Using default file_path=${file_path}`)
 }
 
 const file = fs.createReadStream(file_path)
@@ -20,6 +25,7 @@ const reader = new wav.Reader()
 file.pipe(reader)
 
 reader.on('format', format => {
+	console.log('format:', format)
 	const dds = new DtmfDetectionStream(format)
 
 	dds.on('digit', digit => {
